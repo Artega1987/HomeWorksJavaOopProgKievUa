5910327972dbac67198e60628af5843769e6c620
Artega1987 <32770423+Artega1987@users.noreply.github.com>
2018-01-29 12:11:01 +0200

    Task # 1 of the OOP course, prog.kiev.ua

---
+++ src/homeWorks1/Main.java
@@ -0,0 +1,19 @@
+package homeWorks1;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Cat cat1 = new Cat("Alisa", 2, 3, 2);
+        Cat cat2 = new Cat("Leva", 1, 2, 3);
+        Cat cat3 = new Cat("Mysa", 4, 5, 7);
+        System.out.println(cat3.fight(cat2));
+        System.out.println(cat3.fight(cat1));
+        System.out.println(cat2.fight(cat1));
+    }
+
+}
