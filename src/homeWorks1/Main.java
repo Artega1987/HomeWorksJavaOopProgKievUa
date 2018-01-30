a53ca15457f949439f01a8841eafedd082c834a3
Artega1987 <32770423+Artega1987@users.noreply.github.com>
2018-01-30 11:14:45 +0200

    There were 2 classes (Cat and Main), put everything in the Main class

--- src/homeWorks1/Main.java
+++ src/homeWorks1/Main.java
@@ -15,4 +15,36 @@ public class Main {
         System.out.println(cat2.fight(cat1));
     }
 
+    public static class Cat {
+        String name;
+        int age;
+        int weight;
+        int strength;
+
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+
+            
+
+        public Cat() {
+        }
+
+        public boolean fight(Cat anotherCat){
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2;
+        }
+
+
+
+
+    }
 }
